26e23a243ebbbb690e666ecd3342264e6435fb69
Babi <Babi@DESKTOP-5CAPTV4>
2023-05-14 20:40:15 -0300

    começando a coda

---
+++ AutoEscola/src/ProjetoEscola/infoAluno.java
@@ -0,0 +1,29 @@
+package ProjetoEscola;
+
+import javax.swing.JOptionPane;
+
+public class infoAluno {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String idade = JOptionPane.showInputDialog("Qual a idade?");
+		String email = JOptionPane.showInputDialog("Qual o email?");
+		String celular = JOptionPane.showInputDialog("Qual o celular?");
+		String endereco = JOptionPane.showInputDialog("Qual seu endereço?");		
+		String cpf = JOptionPane.showInputDialog("Qual seu CPF?");
+		String rg = JOptionPane.showInputDialog("Qual seu RG?");
+		String dataNascimento = JOptionPane.showInputDialog("Qual sua data de nascimento?");
+		String dataMatricula = JOptionPane.showInputDialog("Qual a data da matricula");
+		String nomeMae = JOptionPane.showInputDialog("Qual nome da mãe?");
+		String nomePai = JOptionPane.showInputDialog("Qual nome do pai?");
+	
+	
+	
+	
+	}
+
+}
+ 	
